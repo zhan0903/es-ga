deb8b2e29bbadde4de7c84c1c348fdb810ffa4ac
zhan0903 <z.han0903@qq.com>
2018-08-17 10:59:47 +1000

    first commit

---
+++ 03_cartpole_ga_my.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python3
+import gym
+import ptan
+import copy
+import numpy as np
+
+import torch
+import torch.nn as nn
+
+from tensorboardX import SummaryWriter
+
+NOISE_STD = 0.01
+POPULATION_SIZE = 1000
+PARENTS_COUNT = 10
+
+
+class Net(nn.Module):
+    def __init__(self, input_shape, n_actions):
+        super(Net, self).__init__()
+
+        self.conv = nn.Sequential(
+            nn.Conv2d(input_shape[0], 32, kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1),
+            nn.ReLU()
+        )
+
+        conv_out_size = self._get_conv_out(input_shape)
+        self.fc = nn.Sequential(
+            nn.Linear(conv_out_size, 512),
+            nn.ReLU(),
+            nn.Linear(512, n_actions)
+        )
+
+    def _get_conv_out(self, shape):
+        o = self.conv(torch.zeros(1, *shape))
+        return int(np.prod(o.size()))
+
+    def forward(self, x):
+        fx = x.float() / 256
+        conv_out = self.conv(fx).view(fx.size()[0], -1)
+        return self.fc(conv_out)
+
+
+def evaluate(env, net):
+    obs = env.reset()
+    reward = 0.0
+    while True:
+        #obs_v = torch.FloatTensor([obs])
+        batch_obs = []
+        batch_obs.append(np.array(obs, copy=False))
+        obs_v = torch.FloatTensor(batch_obs)
+        act_prob = net(obs_v)
+        acts = act_prob.max(dim=1)[1]
+        obs, r, done, _ = env.step(acts.data.numpy()[0])
+        reward += r
+        if done:
+            break
+    return reward
+
+
+def mutate_parent(net):
+    new_net = copy.deepcopy(net)
+    for p in new_net.parameters():
+        noise_t = torch.tensor(np.random.normal(size=p.data.size()).astype(np.float32))
+        p.data += NOISE_STD * noise_t
+    return new_net
+
+
+def make_env():
+    return ptan.common.wrappers.wrap_dqn(gym.make("PongNoFrameskip-v4"))
+
+
+if __name__ == "__main__":
+    writer = SummaryWriter(comment="-pong-ga")
+    env = make_env()#gym.make("CartPole-v0")
+
+    gen_idx = 0
+    nets = [
+        Net(env.observation_space.shape, env.action_space.n)
+        for _ in range(POPULATION_SIZE)
+    ]
+    population = [
+        (net, evaluate(env, net))
+        for net in nets
+    ]
+    while True:
+        population.sort(key=lambda p: p[1], reverse=True)
+        rewards = [p[1] for p in population[:PARENTS_COUNT]]
+        reward_mean = np.mean(rewards)
+        reward_max = np.max(rewards)
+        reward_std = np.std(rewards)
+
+        writer.add_scalar("reward_mean", reward_mean, gen_idx)
+        writer.add_scalar("reward_std", reward_std, gen_idx)
+        writer.add_scalar("reward_max", reward_max, gen_idx)
+        print("%d: reward_mean=%.2f, reward_max=%.2f, reward_std=%.2f" % (
+            gen_idx, reward_mean, reward_max, reward_std))
+        if reward_mean > 18:
+            print("Solved in %d steps" % gen_idx)
+            break
+
+        # generate next population
+        prev_population = population
+        population = [population[0]]
+        for _ in range(POPULATION_SIZE-1):
+            parent_idx = np.random.randint(0, PARENTS_COUNT)
+            parent = prev_population[parent_idx][0]
+            net = mutate_parent(parent)
+            fitness = evaluate(env, net)
+            population.append((net, fitness))
+        gen_idx += 1
+
+    pass
