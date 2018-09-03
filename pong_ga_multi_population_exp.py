74774533565aeb191087727e31b1cab9dbcdf5fa
zhan0903 <z.han0903@qq.com>
2018-09-03 16:29:04 +1000

    pong_ga_multi_population_exp version: global parents version:DEBUG

--- pong_ga_multi_population_exp.py
+++ pong_ga_multi_population_exp.py
@@ -188,7 +188,7 @@ if __name__ == "__main__":
         torch.manual_seed(seed)
         share_parent = Net(env.observation_space.shape, env.action_space.n)
         #share_parent.state_dict().share_memory_()
-        share_parents.append(share_parent)#.state_dict())
+        share_parents.append(share_parent.state_dict())
 
     value_d = []
     for l in range(PARENTS_COUNT):
