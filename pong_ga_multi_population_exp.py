817a9a131df26013e7e35c7f5e3d18fa1839c5d4
zhan0903 <z.han0903@qq.com>
2018-09-20 15:51:06 +1000

    9-19version:average all top children

--- pong_ga_multi_population_exp.py
+++ pong_ga_multi_population_exp.py
@@ -248,17 +248,18 @@ def evolve(game, exp, logger):
 
         next_parents = []
         elite_c = []
-
-        for i in range(parents_number):
+        for i in range(len(top_children)):
             new_net = Net(env.observation_space.shape, env.action_space.n)
             new_net.load_state_dict(top_children[i][0])
             p_reward, steps = evaluate(env, new_net, device="cpu", evaluate_episodes=10)
             all_frames = all_frames+steps
             elite_c.append((new_net.cpu().state_dict(), p_reward))
-            next_parents.append(new_net.cpu().state_dict())
-
+            # next_parents.append(new_net.cpu().state_dict())
         elite_c.sort(key=lambda p: p[1], reverse=True)
         elite = copy.deepcopy(elite_c[0])
+        for j in range(parents_number):
+            next_parents.append(elite_c[j][0])
+
         logger.debug("top_children[0] reward:{0}, top_children[1] reward:{1}".format(top_children[0][1],
                                                                                      top_children[1][1]))
         logger.debug("next_parents[0]:{0},next_parents[1]:{1}".format(next_parents[0]['fc.2.bias'],
